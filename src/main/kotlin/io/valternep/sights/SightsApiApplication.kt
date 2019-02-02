c37a3abe7201e81237f40562fcce732ae766ad60
Valter Nepomuceno <valter.nep@gmail.com>
2019-02-02 21:04:06 +0000

    Bootstrap Spring Boot application

---
+++ src/main/kotlin/io/valternep/sights/SightsApiApplication.kt
@@ -0,0 +1,11 @@
+package io.valternep.sights
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SightsApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SightsApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/valternep/sights/SightsApiApplicationTests.kt
@@ -0,0 +1,14 @@
+package io.valternep.sights
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SightsApiApplicationTests {
+
+    @Test
+    fun contextLoads() {}
+}
