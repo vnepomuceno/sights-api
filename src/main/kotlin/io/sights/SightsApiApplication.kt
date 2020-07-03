70856f56f7df372c36b13b06e0804101b7c5e9eb
Valter Nepomuceno <valter.nep@gmail.com>
2020-07-03 23:10:27 +0100

    Add Spring Boot Admin

--- src/main/kotlin/io/sights/SightsApiApplication.kt
+++ src/main/kotlin/io/sights/SightsApiApplication.kt
@@ -1,8 +1,10 @@
 package io.sights
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableAdminServer
 @SpringBootApplication
 class SightsApiApplication
 
